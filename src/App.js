fb081520524188f73469132041dddac641f36697
tonij2020 <69843584+naccyworld@users.noreply.github.com>
2021-09-03 15:18:55 +0100

    app.js-update

--- src/App.js
+++ src/App.js
@@ -43,8 +43,8 @@ export default class App extends Component {
             <div className="container d-flex align-items-center flex-column">
               <Switch>
                 <Route exact path="/" component={() => <Home user={this.state.user} />} />
-                {/* <Route exact path="/home" component={() => <Home user={this.state.user} />} /> */}
                 <Route exact path="/login" component={() => <LoginForm setUser={this.setUser} />} />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/home" component={Home} />
                 <Route exact path="/register" component={RegistrationForm} />
                 <Route exact path="/projects" component={Projects} />
